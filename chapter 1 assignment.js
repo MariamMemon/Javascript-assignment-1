8650a89dcdd248272b7bd93a8b6a17f9b641d64b
Mariam Memon <126472943+MariamMemon@users.noreply.github.com>
2023-10-06 21:29:34 -0700

    Add files via upload

---
+++ chapter 1 assignment.js
@@ -0,0 +1,17 @@
+alert("Welcome!");
+
+// 1.4
+
+let a = 10; //value of a is 10
+console.log(a) //it will print value of a
+
+/* This is 
+our first
+javascript 
+assignment */
+
+// Chapter project
+
+// console.log("Mariam");
+
+/* Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, quod! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et deserunt, cum in asperiores aliquam repellat. Quo magnam, ipsum ipsam aliquam iste, necessitatibus quas, alias delectus ea animi distinctio consectetur eligendi ipsa accusamus dicta impedit veritatis dolore reiciendis! Odit dolor, sapiente placeat adipisci voluptatem quasi iusto quae non asperiores, vero impedit. */
